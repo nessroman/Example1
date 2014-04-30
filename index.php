5caadbefed9ae4e505d617dfdddab45b024bf20f
Ness Roman <nessroman@gmail.com>
2014-04-30 12:19:25 -0500

    Ejemplo.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>index</title>
+		<meta name="author" content="ness" />
+		<!-- Date: 2014-04-30 -->
+	</head>
+	<body>
+	<?php 
+		// Esto es un ejemplo para que puedan ir agregando código aquí y enviando los commits
+		
+	?>
+	</body>
+</html>
+
